8617d2873d0a2801d0e2dd28a937750122477c21
fernando Schilipack <nando_fernendo@hotmail.com>
2022-03-10 11:00:18 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsigTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultarMargen();
+            Console.ReadKey(); 
+        }
+
+
+        private static void ConsultarMargen()
+        {
+            var objetoRequest = new Request();
+            var json = JsonConvert.SerializeObject(objetoRequest);
+            var certificate = new X509Certificate2();
+            certificate.Import(@"C:\Users\nando\Dropbox\PC\Documents\ALGORIX SERVIÇO\Repos tasks Joao\CertificadoConsig\somaBnk.pfx", "algx135", X509KeyStorageFlags.DefaultKeySet);
+            var request = (HttpWebRequest)WebRequest.Create("https://webservice-v3.consignet.com.br:8197/consultar-margem");
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate2,
+            X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            { return true; };
+            request.ClientCertificates.Add(certificate);
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    var parse = JObject.Parse(result);
+                    foreach (var item in parse)
+                    {
+                        Console.WriteLine("{0}: {1}", item.Key, item.Value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsigTeste
+{
+
+    public class Request
+    {
+        public string usuarioWebservice { get; set; } = "INTEGRACAO@SOMAFINANCEIRA";
+        public string senhaWebservice { get; set; } = "INTEGRACAO";
+        public string cnpjOrgao { get; set; } = "94436474000124";
+        public string cpfColaborador { get; set; } = "72090219220";
+    }
+
+    public class Response
+    {
+        public List<Margen> margens { get; set; }
+    }
+
+    public class Margen
+    {
+        public string identificadorUnicoMargem { get; set; }
+        public string cnpjOrgao { get; set; }
+        public string matriculaColaborador { get; set; }
+        public double margemDisponivel { get; set; }
+        public int diaCorte { get; set; }
+        public int folhaVigente { get; set; }
+        public bool bloqueado { get; set; }
+        public string motivoBloqueio { get; set; }
+        public string vinculo { get; set; }
+        public string tipoMargem { get; set; }
+        public string dataAdmissao { get; set; }
+        public object dataFinalVinculo { get; set; }
+        public double margemTotal { get; set; }
+        public string dataNascimento { get; set; }
+        public string nomeColaborador { get; set; }
+        public string situacaoFuncional { get; set; }
+        public int codigoIntegracaoFolha { get; set; }
+    }
+}
